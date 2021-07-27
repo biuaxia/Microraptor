d41d195d423b849dee926e6d270d8ab058f9db80
biuaxia <biuaxia@qq.com>
2021-07-27 14:33:57 +0800

    chore: 增加 gradle 相关配置文件

---
+++ src/main/java/cn/biauxia/code/microraptor/MicroraptorApplication.java
@@ -0,0 +1,13 @@
+package cn.biauxia.code.microraptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroraptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroraptorApplication.class, args);
+	}
+
+}
